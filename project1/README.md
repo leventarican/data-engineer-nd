6c399e81e2d46869df3a46d8bdf482ffa0d9045b
leventarican <levent@protonmail.com>
2020-12-07 23:58:01 +0100

    adapt review results

--- project1/README.md
+++ project1/README.md
@@ -1,10 +1,34 @@
 # project 1
+
+## Project Overview
+a basic description of the project. \
+we have two type JSON sources: song data and log data. \
+this project is to bring the JSON sources into a relational database (= etl).
+
+## Project Repository files
+* `create-tables.py`: create the database, drop previous tables and create tables
+* ``
+
+## How To Run the Project
+
+## database and analytics goals
+we created a relational database (postgres) in order to provide an easy way to query the data. 
+
+## database schema design
+* we have one fact table: _songplays_. rest of the tables are dimension tables
+* the design relies on the star schema. the fact tables has references (foreign keys) to dimension tables. by doing this we have relations to the tables.
+
+## ETL pipeline
+to bring our data (JSON files) to database we do ETL (extract, transform and load): \
+* extract: load the data from songs and log data (JSON files)
+* transform: distribute data to specific tables with dataframe, time transformation, ...
+* load: insert into database
+
+## out of scope
+__notes__
 * JSON songs meta data resides under `songs_data/`
 * JSON user activity (on app) data resides under `log_data/`
 
-* create Postgres database
-* create tables; create database schemas
-
-## definitions
+__definitions__
 * _relational schema_: a logical definition of a table. like a blueprint.
 * _database schema_: collection of the relational schemas for the hole database
--- project1/sql_queries.py
+++ project1/sql_queries.py
@@ -1,25 +1,67 @@
 
-# fact table
-# ##############################################################################
-# songplays - records in log data associated with song plays i.e. records with page NextSong
-# songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+"""
+fact table
+################################################################################
+songplays - records in log data associated with song plays i.e. records with
+page NextSong
+songplay_id, start_time, user_id, level, song_id, artist_id, session_id,
+location, user_agent
 
-# dimension tables
-# ##############################################################################
-# users - users in the app
-# user_id, first_name, last_name, gender, level
+dimension tables
+################################################################################
+users - users in the app
+user_id, first_name, last_name, gender, level
 
-# songs - songs in music database
-# song_id, title, artist_id, year, duration
+songs - songs in music database
+song_id, title, artist_id, year, duration
 
-# artists - artists in music database
-# artist_id, name, location, latitude, longitude
+artists - artists in music database
+artist_id, name, location, latitude, longitude
 
-# time - timestamps of records in songplays broken down into specific units
-# start_time, hour, day, week, month, year, weekday
+time - timestamps of records in songplays broken down into specific units
+start_time, hour, day, week, month, year, weekday
+
+example song data
+################################################################################
+{
+    "num_songs": 1,
+    "artist_id": "ARMJAGH1187FB546F3",
+    "artist_latitude": 35.14968,
+    "artist_longitude": -90.04892,
+    "artist_location": "Memphis, TN",
+    "artist_name": "The Box Tops",
+    "song_id": "SOCIWDW12A8C13D406",
+    "title": "Soul Deep",
+    "duration": 148.03546,
+    "year": 1969
+}
+
+example log data
+################################################################################
+{
+    "artist": null,
+    "auth": "Logged In",
+    "firstName": "Kaylee",
+    "gender": "F",
+    "itemInSession": 2,
+    "lastName": "Summers",
+    "length": null,
+    "level": "free",
+    "location": "Phoenix-Mesa-Scottsdale, AZ",
+    "method": "GET",
+    "page": "Upgrade",
+    "registration": 1540344794796.0,
+    "sessionId": 139,
+    "song": null,
+    "status": 200,
+    "ts": 1541106132796,
+    "userAgent": Gecko
+    "userId": "8"
+}
+"""
 
 # DROP TABLES
-
+# ##############################################################################
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
@@ -27,125 +69,95 @@ artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-
-# example song data
-# {
-#     "num_songs": 1,
-#     "artist_id": "ARMJAGH1187FB546F3",
-#     "artist_latitude": 35.14968,
-#     "artist_longitude": -90.04892,
-#     "artist_location": "Memphis, TN",
-#     "artist_name": "The Box Tops",
-#     "song_id": "SOCIWDW12A8C13D406",
-#     "title": "Soul Deep",
-#     "duration": 148.03546,
-#     "year": 1969
-# }
-
-# example log data
-# {
-#     "artist": null,
-#     "auth": "Logged In",
-#     "firstName": "Kaylee",
-#     "gender": "F",
-#     "itemInSession": 2,
-#     "lastName": "Summers",
-#     "length": null,
-#     "level": "free",
-#     "location": "Phoenix-Mesa-Scottsdale, AZ",
-#     "method": "GET",
-#     "page": "Upgrade",
-#     "registration": 1540344794796.0,
-#     "sessionId": 139,
-#     "song": null,
-#     "status": 200,
-#     "ts": 1541106132796,
-#     "userAgent": "\"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/35.0.1916.153 Safari\/537.36\"",
-#     "userId": "8"
-# }
-
-
-
-# songplays - records in log data associated with song plays i.e. records with page NextSong
-# songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+# ##############################################################################
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id int, start_time varchar, user_id int, level varchar, 
-    song_id int, artist_id int, session_id int, location varchar, user_agent varchar
+    songplay_id int, start_time varchar, user_id int, level varchar,
+    song_id varchar, artist_id varchar, session_id int, location varchar,
+    user_agent varchar
 );
 """)
 
-# users - users in the app
-# user_id, first_name, last_name, gender, level
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id int, first_name varchar, last_name varchar, gender varchar, level varchar
+    user_id int, first_name varchar, last_name varchar, gender varchar,
+    level varchar
 );
 """)
 
-# songs - songs in music database
-# song_id, title, artist_id, year, duration
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-    song_id varchar, title varchar, artist_id varchar, year int, duration numeric
+    song_id varchar, title varchar, artist_id varchar, year int,
+    duration numeric
 );
 """)
 
-# artists - artists in music database
-# artist_id, name, location, latitude, longitude
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id int, name varchar, location varchar, latitude numeric, longitude numeric
+    artist_id varchar, name varchar, location varchar, latitude numeric,
+    longitude numeric
 );
 """)
 
-# time - timestamps of records in songplays broken down into specific units
-# start_time, hour, day, week, month, year, weekday
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-    start_time varchar, hour int, day int, week int, month int, year int, weekday int
+    start_time varchar, hour int, day int, week int, month int, year int,
+    weekday int
 );
 """)
 
 # INSERT RECORDS
+# ##############################################################################
 
-# ['ARD7TVE1187B99BFB1',
-#  nan,
-#  'California - LA',
-#  nan,
-#  'Casual',
-#  218.93179,
-#  1,
-#  'SOMZWCG12A8C13C480',
-#  "I Didn't Mean To",
-#  0]
-
-# songplays - records in log data associated with song plays i.e. records with page NextSong
-# songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
 songplay_table_insert = ("""
+INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id,
+artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s);
 """)
 
-# songs - songs in music database
-# song_id, title, artist_id, year, duration
 song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s);
 """)
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
+# ##############################################################################
 
 song_select = ("""
+SELECT song_id, artists.artist_id FROM (
+    songs
+    JOIN artists ON songs.artist_id = artists.artist_id
+) WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
+;
 """)
 
 # QUERY LISTS
+# ##############################################################################
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop]
